7ec46cc1de6c6ad2b4b4b9743db9cb0ff23da833
Nehailaa <93628865+Nehailaa@users.noreply.github.com>
2022-02-08 00:23:59 -0600

    notes.html & api.js-save notes

--- Routes/api.js
+++ Routes/api.js
@@ -1,19 +1,25 @@
 // Modules
 const router = require('express').Router();
-const db = require('../db/db.json');
+let db = require('../db/db.json');
+const fs = require("fs");
 
-// Recouping Notes
-router.get('notes', (req, res) => {
+// Getting Notes
+router.get('/notes', (req, res) => {
     res.status(200).json(JSON.parse(db.getNotes()));
 });
 
-// Creating Notes 
+// Posting Notes 
 router.post('/notes', (req, res) => {
     db
         .addNote(req.body)
         .then(note => res.json(note))
         .catch(err => res.status(400).json(err))
 });
+
+// Saving Notes
+router.put('/notes', (req, res) => {
+    db.saveNotes(JSON.stringify(notes));
+})
 
 // Deleteing Notes 
 router.delete('/notes/:id', (req, res) => {
